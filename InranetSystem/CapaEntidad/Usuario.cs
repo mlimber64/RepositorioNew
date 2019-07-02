994397a6fe3ad278ca79bd6bb4a020615c5a52d0
Limber Mendoza Tolentino <limber_21_97@hotmail.com>
2019-07-02 16:21:56 -0500

    Solucion Completa

---
+++ InranetSystem/CapaEntidad/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Usuario
+    {
+        public int idUsuario { set; get; }
+        public string nomUsuario { set; get; }
+        public string Contrasena { set; get; }
+        public string Nombre { set; get; }
+        public string ApellidoCompleto { set; get; }
+        public string Direccion { set; get; }
+        public string Telefono { set; get; }
+    }
+}
